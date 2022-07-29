4d66aaba40a681f7fcc75c36722068225d8d05b7
Кристина Ратник <kristi.2001@yandex.ru>
2022-07-29 17:33:33 +0300

    fix users views

--- users/views.py
+++ users/views.py
@@ -1,18 +1,10 @@
-import json
-
-from django.core.exceptions import ValidationError
-from django.core.paginator import Paginator
-from django.db.models import Count, Q
 from django.http import JsonResponse
 
 # Create your views here.
-from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
-from homework_29 import settings
-from users.models import User, Location
+from users.models import User
 from users.serializers import UserSerializer, UserCreateSerializer, UserUpdateSerializer, UserDestroySerializer
 
 
