78b3bd892aa38e26ed173f23f213880a7e60706b
Кристина Ратник <kristi.2001@yandex.ru>
2022-08-07 14:48:56 +0300

    fix path

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.index),
     path('user/', views.UserListView.as_view(), name='users'),
     path("user/<int:pk>/", views.UserDetailView.as_view(), name='user'),
-    path("user/create/", views.UserCreateView.as_view(), name='create_user'),
-    path("user/<int:pk>/update/", views.UserUpdateView.as_view(), name='update_user'),
-    path("user/<int:pk>/delete/", views.UserDeleteView.as_view(), name='delete_user'),
+    path("create/", views.UserCreateView.as_view(), name='create_user'),
+    path("<int:pk>/update/", views.UserUpdateView.as_view(), name='update_user'),
+    path("<int:pk>/delete/", views.UserDeleteView.as_view(), name='delete_user'),
 ]
